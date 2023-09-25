558f258230ccf1b8d49d2075a3315071112f95ee
Kenny <1841819+kenny-f@users.noreply.github.com>
2023-09-25 12:39:10 +0100

    add panda css to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -14,6 +15,16 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: async (config, { configType }) => {
+    const {
+      resolve = {}
+    } = config
+    resolve.plugins = [...(config.resolve!.plugins || []), new TsconfigPathsPlugin({
+      extensions: config.resolve!.extensions
+    })];
+    
+    return config;
   },
 };
 export default config;
--- src/app/basic-form/BasicForm.tsx
+++ src/app/basic-form/BasicForm.tsx
@@ -1,9 +1,9 @@
+import { css } from '@styled-system/css'
 import React from 'react'
 interface BasicFormProps {
 }
 export const BasicForm: React.FC<BasicFormProps> = () => {
-console.log
-return (
-<div>Hello</div>
-)
+  return (
+    <div className={css({fontSize: '4xl'})}>Hello</div>
+  )
 }
